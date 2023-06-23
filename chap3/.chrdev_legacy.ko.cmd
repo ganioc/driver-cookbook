059d241c9306d06c713f0be8f9a6537a4785774f
yango <o3.yang@hotmail.com>
2023-06-23 22:52:40 +0800

    update .gitignore

---
+++ chap3/.chrdev_legacy.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/yango/project/driver-cookbook/chap3/chrdev_legacy.ko := aarch64-linux-gnu-ld -r -EL  -maarch64elf -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds  --build-id  -o /home/yango/project/driver-cookbook/chap3/chrdev_legacy.ko /home/yango/project/driver-cookbook/chap3/chrdev_legacy.o /home/yango/project/driver-cookbook/chap3/chrdev_legacy.mod.o ;  true
---
+++ chap3/.tmp_versions/chrdev_legacy.mod
@@ -0,0 +1,3 @@
+/home/yango/project/driver-cookbook/chap3/chrdev_legacy.ko
+/home/yango/project/driver-cookbook/chap3/chrdev_legacy.o
+
